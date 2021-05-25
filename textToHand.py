1f20a49e09eb644dbfdb8c34177e377cc3602826
jagriti204006 <84565473+jagriti204006@users.noreply.github.com>
2021-05-26 02:05:36 +0530

    Add files via upload

---
+++ textToHand.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("My name is Jagriti Malhotra. I am currently pursuing BSc Computer Science hons from Sri Guru Gobind Singh College For Commerce", rgb =(0,0,0))
